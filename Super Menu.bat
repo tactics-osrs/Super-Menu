ed085470710a5768dde0ef585aa778f5c4059a31
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2023-12-29 13:26:32 -0500

    Update Super Menu.bat
    Added Shutdown warnings and a "C" option to cancel any shutdown/restart command within the allotted 30 seconds i also added onto each "Shutdown" command line making it more user friendly and accident safe

--- Super Menu.bat
+++ Super Menu.bat
@@ -16,16 +16,45 @@ echo 7. Volume Mixer
 echo 8. Open Control Panel
 echo 9. Ping Google
 echo 0. Turn Off Computer
+echo C. Cancel Shutdown
 echo.
 set /p choice="Enter your Choice: "
-if "%choice%"=="1" shutdown /r /t 0
-if "%choice%"=="2" shutdown /r /fw /t 0
-if "%choice%"=="3" shutdown /l
+
+if "%choice%"=="1" (
+echo You have 30 seconds before the computer will reset.
+shutdown /r /t 30
+pause
+)
+if "%choice%"=="2" (
+echo You have 30 seconds before the UEFI Firmware Settings will be entered.
+shutdown /r /fw /t 30
+pause
+)
+if "%choice%"=="3" (
+echo You have 30 seconds before you will be signed out.
+shutdown /l /t 30
+pause
+)
 if "%choice%"=="4" rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+
 if "%choice%"=="5" start taskmgr
+
 if "%choice%"=="6" start ms-settings:network-wifi
+
 if "%choice%"=="7" start sndvol.exe
+
 if "%choice%"=="8" start control panel
+
 if "%choice%"=="9" ping www.google.com
-if "%choice%"=="0" shutdown /s /t 0
+
+if "%choice%"=="0" (
+echo You have 30 seconds before the computer will turn off.
+shutdown /s /t 30
+pause
+)
+if /I "%choice%"=="C" (
+echo Cancelling scheduled shutdown...
+shutdown /a
+Pause
+)
 goto menu
